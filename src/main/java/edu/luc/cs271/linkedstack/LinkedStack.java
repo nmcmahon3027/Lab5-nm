480072d4b5280820092f3477f3d6692b64c69da4
nmcmahon <nmcmahon@luc.edu>
2018-07-03 19:18:08 -0500

    Lab 5 Answers:
    3.
    a. Because Java will already have a constructor (implicit) , especially if the programmer
    does not write one themselves. Java creates a constuctor already (behind the scenes) if a no argument
    constructor is implimented; java will then invoke this constructor it has made. Inhereitance is
    part of this too. Each class can extrend anothers, thus this particular class does'nt need
    a implicit constructor. LinkedStack inherites from IStack and extends it.
    
    b. It is the total stack space that has been used, in our case however many we add.
    It is the extra space or temporary space used by that algorithm, the one that creates the stack
    and adds to it.
    
    c. You could create a list rotation method. If it is negative the list/array that you want to
    re-create would move each element to the left. As long as the count/number of
    rotations is less than 0, you could add to the count by list.size();
    Also you would later create a new LinkedList (maybe?) and do Arrays.asList then
    do a while loop to execute while loop is true and rotate the list.
    
    d. For me, personally, I had more success with pushing in stacks. That way you
    dont have to worry about deleting an element from the stack. If I was using pop,
    I would do an if(isEmpty) throw new NoSuchElementException ....
    then have the first node of the stack be linked to the item and then first.next;
    Pop just has a more moving parts to it than a simple push.

--- src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
+++ src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
@@ -2,43 +2,114 @@ package edu.luc.cs271.linkedstack;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Arrays;
 
 
 public class LinkedStack<E> implements IStack<E> {
 
-  /** The topmost node of this stack. The stack gets pushed down from here. */
-  private Node<E> top;
+  /**
+   * The topmost node of this stack. The stack gets pushed down from here.
+   */
+  //Mine below***
+  private Node<E> topOfStackRef = null;
 
-  // TODO why don't we need an explicit constructor?
+  // *Q: TODO why don't we need an explicit constructor?
 
   @Override
   public E push(final E obj) {
-    // TODO
-    return null;
+    // TODO*
+    topOfStackRef = new Node<>(obj, topOfStackRef);
+
+    return obj;
   }
 
   @Override
   public E peek() {
-    // TODO
-    return null;
+    // TODO*
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    } else {
+      return topOfStackRef.data;
+    }
+
   }
 
   @Override
   public E pop() {
-    // TODO
-    return null;
+    // TODO*
+    E result;
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    } else {
+      result = topOfStackRef.data;
+      topOfStackRef = topOfStackRef.next;
+    }
+    return result;
   }
+
+
 
   @Override
   public boolean isEmpty() {
-    // TODO
-    return false;
+    // TODO*
+
+    return topOfStackRef == null;
   }
 
   @Override
-  public List<E> asList() {
+  public List<E> asList() {//me below
+   /* String[] stackArray;
+    int stackSize;
+    int topOfStack = -1;
+
+    TheStack(int size){
+      stackSize = size;
+      stackArray = new String[size];
+      Arrays.fill(stackArray, "-1");
+
+    }
+    public void push(String input){
+      if(topOfStack +1 < stackSize){
+        topOfStack++;
+        stackArray[topOfStack] = input;
+
+      }else System.out.println("Stack is full!");
+      displayTheStack();
+      displayTheStack();
+      System.out.println("PUSH" + input + "was added to the stack\n");
+
+    }
+    private void displayTheStack(){
+      for(int n = 0; n< 61; n++) System.out.print("-");
+      System.out.println();
+      for(int n = 0; n< stackSize; n++){
+        System.out.format("| %2s "+ " ", n);
+      }
+      System.out.println("|");
+      for(int n = 0; n< 61; n++)System.out.print("-");
+      System.out.println();
+      for(int n= 0; n< stackSize; n++){
+        if(stackArray[n].equals("-1")) System.out.print("|        ");
+        else System.out.print(String.format("| %2s " + " ", stackArray[n]));
+      }
+      System.out.println("|");
+      for(int n = 0; n< 61; n++)System.out.print("-");
+      System.out.println();
+    }
+    public static void main(String[] args){
+      TheStack theStack = new TheStack(10);
+      TheStack.push("10");
+      TheStack.push("15");
+
+      */
+   return null;
+    }
+
+
     // TODO implement using an ArrayList.  Populate the ArrayList with the stack values, without deconstructing the stack!
     // TODO add any instance variable(s) required to support this
-    return null;
+
   }
-}
+
+
--- src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -4,9 +4,8 @@ import java.util.Scanner;
 
 public class ReverseLines {
 
-  public static void main(final String[] args) {
+  public static void main(String args[]) {
     // TODO read successive input lines until EOF, then print out in reverse order
-
 
 
     final Scanner input = new Scanner(System.in);
@@ -14,10 +13,35 @@ public class ReverseLines {
       final String line = input.nextLine();
 
 
+      //Mine below
+
+
+
+      }
+
 
     }
+
+  private static void testA(int a, int b) {
+    a = 0;
+    b = 0;
+    if (a == b) {
+      return;
+    }
+    StringBuilder result = new StringBuilder();
+    for (int i = a; ; i += a < b ? 1 : -1) {
+      result.append(i).append("\t");
+      if (i == b) {
+        break;
+      }
+    }
+    System.out.println(result.deleteCharAt(result.length() - 1).toString());
+
+
+  }
 
 
 
   }
-}
+
+
